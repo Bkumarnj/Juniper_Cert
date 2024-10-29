099e76559df256ac2f5ec207ec46c80923bb49ce
Bhuvanesh Kumar N J <kumar_njb@outlook.com>
2024-10-29 21:33:53 +0530

    Updated junos_config_files README.md

--- gns3_lab_setup/BKNJ-LabGFW-vSRX01_deploy_script/BKNJ-LRF-vSRX01.sh
+++ gns3_lab_setup/BKNJ-LabGFW-vSRX01_deploy_script/BKNJ-LRF-vSRX01.sh
@@ -1,6 +1,6 @@
 qm create 102 --name=BKNJ-LabGFW-vSRX01 --memory=4092 --sockets=1 --cores=2 --balloon=0 \
   --machine pc-q35-8.0 --serial0=socket --scsihw=virtio-scsi-pci --cpu=host --ostype=l26 \
-  --net0=virtio,bridge=vmbr0 --net1=virtio,bridge=vmbr1 --net2=virtio,bridge=vmbr2 --net3=virtio,bridge=vmbr1 --net4-virtio,bridge=vmbr3
+  --net0=virtio,bridge=vmbr0 --net1=virtio,bridge=vmbr1 --net2=virtio,bridge=vmbr2 --net3=virtio,bridge=vmbr1 --net4=virtio,bridge=vmbr3
 
 qm set 102 -args '-machine smbios-entry-point-type=32'
 
